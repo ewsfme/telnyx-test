d1d6bccc207ab685e39c6dc1c8ceff9c9e047eed
D <quodexcitare@proton.me>
2026-07-12 20:25:16 +0200

    fix2

--- cypress/e2e/TC_07_pricing_navigation.cy.js
+++ cypress/e2e/TC_07_pricing_navigation.cy.js
@@ -1,11 +1,14 @@
 import mainPage from '../pageObjects/MainPage';
 
-describe('TC-07: Pricing Navigation', () => {
-  it('Verify Pricing Link Redirects to Correct Page', () => {
+describe('TC_07: Why Telnyx Navigation', () => {
+  it('Verify Why Telnyx Link Redirects to Correct Page', () => {
     mainPage.visit();
     
-    mainPage.clickPricing();
+    mainPage.getNavItem('Why Telnyx')
+      .should('be.visible')
+      .click();
     
-    cy.url({ timeout: 10000 }).should('include', '/pricing');
+    cy.url({ timeout: 10000 })
+      .should('include', '/why-telnyx');
   });
 });
