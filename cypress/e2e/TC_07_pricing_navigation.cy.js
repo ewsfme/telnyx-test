93def1fcadb4d6ee112d16cebe173eed5e276b38
D <quodexcitare@proton.me>
2026-07-12 19:24:50 +0200

    fix

--- cypress/e2e/TC_07_pricing_navigation.cy.js
+++ cypress/e2e/TC_07_pricing_navigation.cy.js
@@ -1,12 +1,11 @@
 import mainPage from '../pageObjects/MainPage';
 
-describe('TC-15: Pricing Navigation', () => {
+describe('TC-07: Pricing Navigation', () => {
   it('Verify Pricing Link Redirects to Correct Page', () => {
     mainPage.visit();
     
     mainPage.clickPricing();
     
-    // Перевіряємо, що після кліку URL змінився на сторінку ціноутворення
     cy.url({ timeout: 10000 }).should('include', '/pricing');
   });
 });
