3b7193e8f82d34c67d6856c49654465ed5b77688
D <quodexcitare@proton.me>
2026-07-12 19:12:20 +0200

    upd

--- cypress/e2e/TC_06_products_navigation.cy.js
+++ cypress/e2e/TC_06_products_navigation.cy.js
@@ -1,9 +1,9 @@
 import mainPage from '../pageObjects/MainPage';
 
-describe('TC-21: Products Navigation Element', () => {
+describe('TC-06: Products Navigation Element', () => {
   it('Verify Products Navigation Item text and state', () => {
     mainPage.visit();
-    
+
     mainPage.getNavItem('Products')
     mainPage.getNavItem('Products').should('exist').and('contain.text', 'View all products');
   });
